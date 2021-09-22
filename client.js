c5baa3c639b25ab51bca6078cd3a5fae3f2fa757
hlai16 <gloriajoanlai@gmail.com>
2021-09-22 14:52:55 -0400

    Snake can move with assistance. Turns out I need to read better

--- client.js
+++ client.js
@@ -1,10 +1,9 @@
 const net = require("net");
-
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: '135.23.223.133',
-    port: '50542',
+    port: 50542,
   });
 
   // interpret incoming data as text
@@ -12,19 +11,36 @@ const connect = function () {
 
   conn.on('connect', () => {
     console.log('Joan is in da house!');
+    conn.write('Name: JL');
+    setTimeout(() => conn.write('Move: up'), 1000);
+    setTimeout(() => conn.write('Move: up'), 1500);
+    setTimeout(() => conn.write('Move: left'), 2000);
   });
+
+
+  conn.on('connect', () => {
+    conn.write('Move: up');
+  });
+
+//   conn.on('connect', () => {
+//     conn.write('Move: up');
+//   });
+
+//   conn.on('connect', () => {
+//     conn.write('Move: left');
+//   });
 
   conn.on('data', (data) => {
     console.log("DATA CAME IN!!!!!");
-    console.log(data);
-})
+    console.log(`Server says: ${data}`);
+  });
+
 
   return conn;
 };
 
 console.log("Connecting ...");
-connect();
 
 module.exports = {
-    connect
+    connect,
 }
