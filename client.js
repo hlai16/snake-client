b8e0cb7b3aacc21f6434cc4981f9df820cccb709
hlai16 <gloriajoanlai@gmail.com>
2021-09-22 13:34:00 -0400

    only able to console.log message when connection is established

--- client.js
+++ client.js
@@ -10,9 +10,8 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log('I connect now');
+  conn.on('connect', () => {
+    console.log('Joan is in da house!');
   });
 
   conn.on('data', (data) => {
