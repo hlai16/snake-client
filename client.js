74e3fca5886cd6ffef4f93921dc8931c86f07d11
hlai16 <gloriajoanlai@gmail.com>
2021-09-22 17:56:16 -0400

    stdin setup completed with assistance.

--- client.js
+++ client.js
@@ -1,6 +1,14 @@
 const net = require("net");
+const { stdin } = require("process");
 // establishes a connection with the game server
 const connect = function () {
+  const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    return stdin;
+      };
   const conn = net.createConnection({
     host: '135.23.223.133',
     port: 50542,
@@ -9,31 +17,40 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  
+
   conn.on('connect', () => {
     console.log('Joan is in da house!');
     conn.write('Name: JL');
-    setTimeout(() => conn.write('Move: up'), 1000);
-    setTimeout(() => conn.write('Move: up'), 1500);
-    setTimeout(() => conn.write('Move: left'), 2000);
   });
-
 
   conn.on('connect', () => {
-    conn.write('Move: up');
+    const moves = ['Move: up', 'Move: up', 'Move: left', 'Move: left', 'Move: up'];
+
+    let delay = 0;
+
+    for (const move of moves) {
+    setTimeout(() => {
+    conn.write(move), delay;
+   },
+    delay += 500);
+   }
   });
-
-//   conn.on('connect', () => {
-//     conn.write('Move: up');
-//   });
-
-//   conn.on('connect', () => {
-//     conn.write('Move: left');
-//   });
 
   conn.on('data', (data) => {
     console.log("DATA CAME IN!!!!!");
     console.log(`Server says: ${data}`);
   });
+
+  const handleUserInput = function () {
+    // your code here
+    if (key === '\u0003') {
+      console.log('disconnecting');
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
 
 
   return conn;
